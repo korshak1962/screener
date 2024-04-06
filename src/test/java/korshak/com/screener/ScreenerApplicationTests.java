0a63267e02639f30ea9ed4e4ca58e91e99d9158c
korsh <korshak1962@gmail.com>
2024-04-06 14:21:07 -0700

    initial commit

---
+++ src/test/java/korshak/com/screener/ScreenerApplicationTests.java
@@ -0,0 +1,13 @@
+package korshak.com.screener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
